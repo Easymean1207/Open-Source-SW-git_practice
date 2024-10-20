18aa310a31ff2965e0ee6d5b3a3a714539f3f293
Easymean1207 <dlwlasl1207@gmail.com>
2024-10-20 21:51:37 +0900

    add JsBasic1.js

---
+++ JsBasic1.js
@@ -0,0 +1,12 @@
+// const 타입 (재선언, 재할당 불가능)
+const msg = 'hello, This is const type'
+// msg = 'This is immutable'
+
+// let 타입 (재선언 불가능, 재할당 가능)
+let msg2 = 'This is let type'
+msg2 = 'This is mutable'
+
+console.log(typeof(msg))
+
+let isBool = true
+console.log(typeof(isBool))

df8f116764aadf191dd46301eae484c350f5e9df
Easymean1207 <dlwlasl1207@gmail.com>
2024-10-20 21:53:33 +0900

    add JsBasic3.js

---
+++ JsBasic3.js
@@ -0,0 +1,18 @@
+// 조건문 (Condition)
+let age = 35;
+
+if (age > 30) {
+  console.log('중후한 와인 어떠십니까?');
+} else if (age >= 20) {
+  console.log('청춘의 향기가 가득한 맥주 어떠십니까?');
+} else {
+  console.log('미성년자는 술을 마실 수 없습니다.');
+}
+
+// 반복문 (Loop)
+sum = 0;
+for (let i = 0; i < 11; i++) {
+  sum += i;
+}
+
+console.log(sum);
